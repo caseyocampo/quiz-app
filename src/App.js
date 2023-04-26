85c17e17f67f578265a559ed738c5d35bf810767
Casey Ocampo <caseysoflo@gmail.com>
2023-04-25 21:42:01 -0400

    Remove alt text

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import babyBlueBlob from "./assets/images/baby_blue_blob.svg"
 function App() {
   return (
     <main>
-      <img src={lemonBlob} alt="decorative lemon colored blob fixed to the top right of the screen" className="lemon-blob" />
-      <img src={babyBlueBlob} alt="decorative lemon colored blob fixed to the top right of the screen" className="baby-blue-blob" />
+      <img src={lemonBlob} alt="" className="lemon-blob" />
+      <img src={babyBlueBlob} alt="" className="baby-blue-blob" />
       <div className="start-page">
         <h1>Quizzical</h1>
         <p>Quiz yourself on some good ol' Trivia!</p>
