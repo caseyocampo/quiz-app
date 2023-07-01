e3ddf4b1db8bcefebaa047c514cdb1201a0194da
Casey Ocampo <caseysoflo@gmail.com>
2023-07-01 12:09:35 -0400

    adding random logic for question

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -12,11 +12,49 @@ export default function StartScreen() {
     incorrect_answers: "",
     category: "",
   })
-
   const [isAnswersChecked, setIsAnswersChecked] = useState(false)
+  const [randomAnswers, setRandomAnswers] = useState([])
+
+  function getRandomItem(set) {
+    let items = Array.from(set)
+    return items[Math.floor(Math.random() * items.length)]
+  }
+
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[a[i], a[j]] = [a[j], a[i]]
+    }
+    return a
+  }
+
+  //   function shuffleAnswers() {
+  //     const correctAnswer = questions.results[0].correct_answer
+  //     let incorrectAnswers = questions.results[0].incorrect_answers
+  //     const allAnswers = []
+  //     allAnswers.push(correctAnswer)
+  //     for (let i = 0; i < incorrectAnswers.length; i++) {
+  //       allAnswers.push(incorrectAnswers[i])
+  //     }
+  //     function shuffle(arr) {
+  //       let j = ""
+  //       let x = ""
+  //       let arrIndex = ""
+  //       for (arrIndex = arr.length - 1; arrIndex > 0; arrIndex--) {
+  //         j = Math.floor(Math.random() * (arrIndex + 1))
+  //         x = arr[arrIndex]
+  //         arr[arrIndex] = arr[j]
+  //         arr[j] = x
+  //       }
+  //       return arr
+  //     }
+  //     const results = shuffle(allAnswers)
+  //     setRandomAnswers(results)
+  //   }
 
   function handleClick() {
     setIsStartScreen(prev => !prev)
+    // shuffleAnswers()
   }
 
   function handleCheckAnswers() {
@@ -29,39 +67,10 @@ export default function StartScreen() {
       .then(data => {
         if (data.results) {
           setQuestions(data)
-          //   console.log(questions)
         }
         return { errorCode: data.status }
       })
   }, [])
-
-  const correctAnswer = questions.results[0].correct_answer
-  let incorrectAnswers = questions.results[0].incorrect_answers
-  const allAnswers = []
-  allAnswers.push(correctAnswer)
-
-  for (let i = 0; i < incorrectAnswers.length; i++) {
-    allAnswers.push(incorrectAnswers[i])
-  }
-  console.log(allAnswers)
-
-  //   console.log(`all answers ${allAnswers}`)
-
-  function shuffle(arr) {
-    let j = ""
-    let x = ""
-    let arrIndex = ""
-    for (arrIndex = arr.length - 1; arrIndex > 0; arrIndex--) {
-      j = Math.floor(Math.random() * (arrIndex + 1))
-      x = arr[arrIndex]
-      arr[arrIndex] = arr[j]
-      arr[j] = x
-    }
-    console.log(arr)
-    // return arr;
-  }
-
-  shuffle(allAnswers)
 
   return (
     <section>
@@ -87,7 +96,14 @@ export default function StartScreen() {
           {questions.results.map((question, index) => (
             <div key={`question-container-${index}`}>
               <h2 key={`question-${index}`}>{decode(question.question)}</h2>
-              <fieldset>
+              {/* {question.incorrect_answers} */}
+              <br />
+              {shuffle(question.incorrect_answers.map(answer => decode(`${answer}, `)))}
+              <br />
+              {question.incorrect_answers.map(answer => decode(`${answer}, `))}
+              {/* {`${decode(question.correct_answer)} ${decode(question.incorrect_answers)}`} */}
+
+              {/* <fieldset>
                 <legend className="sr-only">Quiz question:</legend>
                 <input type="radio" id={`one-${index}`} name="quiz" value="huey" />
                 <label htmlFor={`one-${index}`} className={`button ${isAnswersChecked && `correct-answer`}`}>
@@ -108,7 +124,7 @@ export default function StartScreen() {
                   {decode(question.incorrect_answers[2])}
                 </label>
                 <hr className="question-divider" />
-              </fieldset>
+              </fieldset> */}
             </div>
           ))}
 
