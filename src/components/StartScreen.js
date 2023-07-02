f4794bf732798dcd46a67789f6a0446a9b39e3b1
Casey Ocampo <caseysoflo@gmail.com>
2023-07-01 20:05:44 -0400

    move to unused file

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -15,11 +15,6 @@ export default function StartScreen() {
   //   const [isAnswersChecked, setIsAnswersChecked] = useState(false)
   //   const [randomAnswers, setRandomAnswers] = useState([])
 
-  //   function getRandomItem(set) {
-  //     let items = Array.from(set)
-  //     return items[Math.floor(Math.random() * items.length)]
-  //   }
-
   function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1))
@@ -27,30 +22,6 @@ export default function StartScreen() {
     }
     return a
   }
-
-  //   function shuffleAnswers() {
-  //     const correctAnswer = questions.results[0].correct_answer
-  //     let incorrectAnswers = questions.results[0].incorrect_answers
-  //     const allAnswers = []
-  //     allAnswers.push(correctAnswer)
-  //     for (let i = 0; i < incorrectAnswers.length; i++) {
-  //       allAnswers.push(incorrectAnswers[i])
-  //     }
-  //     function shuffle(arr) {
-  //       let j = ""
-  //       let x = ""
-  //       let arrIndex = ""
-  //       for (arrIndex = arr.length - 1; arrIndex > 0; arrIndex--) {
-  //         j = Math.floor(Math.random() * (arrIndex + 1))
-  //         x = arr[arrIndex]
-  //         arr[arrIndex] = arr[j]
-  //         arr[j] = x
-  //       }
-  //       return arr
-  //     }
-  //     const results = shuffle(allAnswers)
-  //     setRandomAnswers(results)
-  //   }
 
   function handleClick() {
     setIsStartScreen(prev => !prev)
---
+++ src/components/unused.js
@@ -0,0 +1,28 @@
+//   function shuffleAnswers() {
+//     const correctAnswer = questions.results[0].correct_answer
+//     let incorrectAnswers = questions.results[0].incorrect_answers
+//     const allAnswers = []
+//     allAnswers.push(correctAnswer)
+//     for (let i = 0; i < incorrectAnswers.length; i++) {
+//       allAnswers.push(incorrectAnswers[i])
+//     }
+//     function shuffle(arr) {
+//       let j = ""
+//       let x = ""
+//       let arrIndex = ""
+//       for (arrIndex = arr.length - 1; arrIndex > 0; arrIndex--) {
+//         j = Math.floor(Math.random() * (arrIndex + 1))
+//         x = arr[arrIndex]
+//         arr[arrIndex] = arr[j]
+//         arr[j] = x
+//       }
+//       return arr
+//     }
+//     const results = shuffle(allAnswers)
+//     setRandomAnswers(results)
+//   }
+
+//   function getRandomItem(set) {
+//     let items = Array.from(set)
+//     return items[Math.floor(Math.random() * items.length)]
+//   }
