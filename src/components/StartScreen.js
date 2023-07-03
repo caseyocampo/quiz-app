b3937d7fbdb079cedf38a798c1de0742c0d7059d
Casey Ocampo <caseysoflo@gmail.com>
2023-07-03 10:05:32 -0400

    update button locations

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -133,12 +133,11 @@ export default function StartScreen() {
 
           <div className="button-container">
             <div className="top-button-container">
-              <button className="button-secondary" onClick={handleReturnToStartPage}>
-                Back to start page
-              </button>
-
               <button className="button-primary" onClick={handleCheckAnswers}>
                 Check answers
+              </button>
+              <button className="button-secondary" onClick={handleReturnToStartPage}>
+                Back to start page
               </button>
             </div>
             <button className="button-secondary bottom-button" onClick={handleNewQuestions}>
