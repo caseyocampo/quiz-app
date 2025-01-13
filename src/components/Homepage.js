5394bc0b8b19aae1cadc68b90ffbd82840aa3eb0
Casey Ocampo <caseysoflo@gmail.com>
2025-01-13 08:59:50 -0500

    chore: update homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,14 +2,10 @@ export default function Homepage({ handleClick }) {
   return (
     <div>
       <div className="start-page">
-        <h1>Quizzical - a Solo Scrimba Project</h1>
-        <p style={{ maxWidth: '500px', textAlign: 'left' }}>
-          To continue my React education, I've joined the{' '}
-          <a href="https://scrimba.com/learn/frontend">Scrimba Front End Development Career Path</a>. We were given a
-          Figma file and list of instructions to complete this assignment. This is the finished project. Enjoy!
-        </p>
+        <h1>Trivia Quiz App</h1>
+        <p className="homepage-instructions">Press the Start Quiz button to begin!</p>
         <button className="button-primary" onClick={handleClick}>
-          Start quiz
+          Start Quiz
         </button>
       </div>
     </div>
